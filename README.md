7c6aca8c7c6570d16cd6f4476d96e135ddef3382
Hugo "ThePooN" Denizart <thepoon@cartooncraft.fr>
2018-01-03 02:28:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# rtmp-fallback
+
+Experimental piece of crap that outputs an RTMP stream that switches to a fallback video file when the RTMP input fails.  
+Basically, it will take an RTMP input and output the RTMP input when possible or the fallback video when it fails.
+
+## Use Cases
+
+- Maintain a Twitch stream alive and not leave viewers with a black screen incase of a technical issue (such as OBS or computer crash or anything of the like)
+- Switching of encoder (as long as the decoder can decode it the same) on the fly
+- **Useful for multi-streamer marathons**: Anyone can take the relay after someone is done (as long as their output settings are the same)
+
+## How It Works
+
+**This tool doesn't re-encode any video, it is extremely light.** That makes it kinda hard to use and not very modular, and this is why you need to understand how it works before using it.  
+RTMP streams are in FLV format. Sadly, FLV files cannot be concatenated at the file level. To work around this, this tool decodes the FLV format **(it doesn't decode the video and audio tracks)** and reassembles them as MPEGTS (.ts) format. This makes concatenating the input stream and fallback videos possible. Then, it reassembles the MPEGTS data as FLV and output it in an RTMP stream.
+
+TL;DR:  
+RTMP FLV data (input stream) -> .TS data (concatenable data) -> Input stream OR Fallback .ts file (that is still .TS data) -> RTMP FLV data
+
+**As there is no decoding/encoding of the video and audio tracks, the parameters of the video and audio tracks from the RTMP stream and fallback file must be identical.**  
+A way to check for that is using `ffprobe` (part of `ffmpeg`). You should look at the video and audio tracks lines for the rtmp input stream and fallback .ts file and make sure they're (nearly) identical. **The only way to make sure the fallback .ts file and the input RTMP stream are compatible is by testing it anyway, and try to play the output feed in multiple players such as VLC and `ffplay`.**  
+Different bitrates and x264 presets should work; different fps may work; different profiles, pixel formats and resolution definitely won't.
+
+## Install
+
+`npm run -g rtmp-fallback`  
+```
+Usage: rtmp-fallback rtmpInput fallbackFile rtmpOutput
+fallbackFile MUST be a .ts file
+Options:
+        -l: Enable logging in /tmp of rtmpdump/ffmpeg outputs.
+        -t ms: Timeout in milliseconds before switching to fallback file. Defaults to 5000ms.
+        -d ms: Set fallback video's duration in ms. Will fallback to automatic detection if not set.
+```
+
+## WARNING
+
+As I said before, it is an experimental tool that I can only recommend advanced users to use in production. It has only been tested on Linux, with `nginx_rtmp_module` with the `wait_video` and `wait_key` parameters enabled.  
+If you're interested in using this tool for your production, throw me a mail or [https://discord.gg/ThePooN](get in touch on Discord).
---
+++ index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+const argv = require("minimist")(process.argv.slice(2));
+const spawn = require("child_process").spawn;
+const fs = require("fs");
+const getDuration = require("get-video-duration");
+
+if(argv._.length < 3) {
+	console.log("Usage: rtmp-fallback rtmpInput fallbackFile rtmpOutput");
+	console.log("fallbackFile MUST be a .ts file");
+	console.log("Options:");
+	console.log("	-l: Enable logging in /tmp of rtmpdump/ffmpeg outputs.");
+	console.log("	-t ms: Timeout in milliseconds before switching to fallback file. Defaults to 5000ms.");
+	console.log("	-d ms: Set fallback video's duration in ms. Will fallback to automatic detection if not set.");
+	process.exit(1);
+}
+
+const timeoutLength = (argv.t) ? argv.t : 5000;
+const loggingEnabled = (argv.l != null);
+
+const noDataBuf = require("fs").readFileSync(process.argv[3]);
+let noDataDur = argv.d;
+if(!noDataDur)
+	getDuration(process.argv[3])
+		.then((duration) => noDataDur = duration*1000)
+		.catch((e) => { 
+			console.error("An error occured while probing duration for fallback file!", e);
+			process.exit(1);
+		});
+let noDataTimeout = null;
+let lastNoData = null;
+
+const ffmpegOut = spawn("ffmpeg", ("-f mpegts -i - -c copy -bsf:a aac_adtstoasc -f flv "+process.argv[4]).split(" "));
+ffmpegOut.on("exit", (code) => { console.log("ffmpegOut exited with code "+code+"! Exiting..."); process.exit(code); });
+
+const ffmpegIn = spawn("ffmpeg", ("-f live_flv -i - -c copy -f mpegts -").split(" "));
+ffmpegIn.on("exit", (code) => { console.log("ffmpegIn exited with code "+code+"! Exiting..."); process.exit(code); });
+ffmpegIn.stdout.on("data", onData);
+
+const currentStream = spawn("rtmpdump", ("-m 0 -v -r "+process.argv[2]).split(" "));
+currentStream.on("exit", (code) => { console.log("rtmpdump exited with code "+code+"+! Exiting..."); process.exit(code); });
+currentStream.stdout.on("data", (videoData) => ffmpegIn.stdin.write(videoData));
+
+if(loggingEnabled) {
+	const ffmpegOutLog = fs.createWriteStream("/tmp/ffmpegoutlog");
+	ffmpegOut.stderr.on("data", (msg) => ffmpegOutLog.write(msg));
+	const ffmpegInLog = fs.createWriteStream("/tmp/ffmpeginlog");
+	ffmpegIn.stderr.on("data", (msg) => ffmpegInLog.write(msg));
+	const rtmpdumpLog = fs.createWriteStream("/tmp/rtmpdumplog");
+	currentStream.stderr.on("data", (msg) => rtmpdumpLog.write(msg));
+}
+
+function onData(videoData) {
+	ffmpegOut.stdin.write(videoData);
+	if(noDataTimeout)
+		clearTimeout(noDataTimeout);
+	noDataTimeout = setTimeout(noData, timeoutLength);
+}
+
+function noData() {
+	console.log("Timeout reached! Switching to fallback file...");
+	ffmpegOut.stdin.write(noDataBuf);
+	setNoDateTimeout();
+}
+
+function setNoDateTimeout() {
+	// Instead of using setInterval, we're using setTimeout with re-calculated delay everytime, otherwise the delay will increase over time
+	// because of the time it takes to write to the console and pass data to ffmpeg
+	noDataTimeout = setTimeout(() => {
+		ffmpegOut.stdin.write(noDataBuf);
+		lastNoData = Date.now();
+		setNoDateTimeout();
+	}, (lastNoData == null) ? noDataDur : (Date.now() - lastNoData + noDataDur));
+}
+
+console.log("RTMP Fallback successfully initialized");
